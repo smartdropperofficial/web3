ed1c1d022af7e5b15bc04ea7458c80509fd2f32d
Bruno Ferreira <bruno.ferreira.rocha@gmail.com>
2025-06-12 19:16:52 +0200

    feat: add token validation middleware to API routes and update payment verification request

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 // src/app.ts
 import express from "express";
 import transactionRoutes from "./routes/transactionsRouter";
+import { ensureToken } from "./middleware/tokenValidation";
 
 const app = express(); // Niente express.json() qui
 
-app.use("/api", transactionRoutes);
+app.use("/api", ensureToken, transactionRoutes);
 
 export default app;
