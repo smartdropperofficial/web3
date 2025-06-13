8d8adf7fb850f1e228bc9607da1b2d1efa2f339a
Bruno Ferreira <bruno.ferreira.rocha@gmail.com>
2025-06-13 16:24:20 +0200

    fix: update server configuration and environment variables for consistency

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,16 @@ import { ensureToken } from "./middleware/tokenValidation";
 
 const app = express();
 
-// Middleware per il parsing del JSON
 app.use(express.json());
 
-// Registriamo il router sotto `/api`
-// app.use("/api", ensureToken, transactionsRouter);
 app.use("/api", ensureToken, transactionsRouter);
 
 console.log("✅ Transactions Router registrato su /api");
 
 // 🔹 Avvia il server SOLO SE siamo in locale
-if (process.env.NODE_ENV !== "vercel") {
-  const PORT = process.env.PORT || 4000;
-  app.listen(PORT, () => {
-    console.log(`✅ Server in esecuzione su http://localhost:${PORT}`);
-  });
-}
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`✅ Server in esecuzione su http://localhost:${PORT}`);
+});
 
 // 🔹 Esportiamo Express per Vercel
